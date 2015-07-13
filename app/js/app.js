4fe80ee00af93e1862abc979a6c8aa8d45f3114b
unknown <sreehari.puliyakkot@MW7C3SLZZZT32X.dir.svc.accenture.com>
2015-07-13 09:56:41 +0100

    some changes I'm not sure what!

--- app/js/app.js
+++ app/js/app.js
@@ -8,51 +8,50 @@ angular.module('uiRouterSample', [
 ])
 
 .run(
-  [          '$rootScope', '$state', '$stateParams',
-    function ($rootScope,   $state,   $stateParams) {
+    ['$rootScope', '$state', '$stateParams',
+      function($rootScope, $state, $stateParams) {
 
-    // It's very handy to add references to $state and $stateParams to the $rootScope
-    // so that you can access them from any scope within your applications.For example,
-    // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
-    // to active whenever 'contacts.list' or one of its decendents is active.
-    $rootScope.$state = $state;
-    $rootScope.$stateParams = $stateParams;
-    }
-  ]
-)
+        // It's very handy to add references to $state and $stateParams to the $rootScope
+        // so that you can access them from any scope within your applications.For example,
+        // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+        // to active whenever 'contacts.list' or one of its decendents is active.
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+      }
+    ]
+  )
+  .config(
+    ['$stateProvider', '$urlRouterProvider',
+      function($stateProvider, $urlRouterProvider) {
 
-.config(
-  [          '$stateProvider', '$urlRouterProvider',
-    function ($stateProvider,   $urlRouterProvider) {
+        /////////////////////////////
+        // Redirects and Otherwise //
+        /////////////////////////////
 
-      /////////////////////////////
-      // Redirects and Otherwise //
-      /////////////////////////////
-
-      // Use $urlRouterProvider to configure any redirects (when) and invalid urls (otherwise).
-      $urlRouterProvider
+        // Use $urlRouterProvider to configure any redirects (when) and invalid urls (otherwise).
+        $urlRouterProvider
 
         // The `when` method says if the url is ever the 1st param, then redirect to the 2nd param
         // Here we are just setting up some convenience urls.
-        .when('/c?id', '/contacts/:id')
-        .when('/user/:id', '/contacts/:id')
+          .when('/c?id', '/contacts/:id')
+          .when('/user/:id', '/contacts/:id')
 
         // If the url is ever invalid, e.g. '/asdf', then redirect to '/' aka the home state
         .otherwise('/');
 
 
-      //////////////////////////
-      // State Configurations //
-      //////////////////////////
+        //////////////////////////
+        // State Configurations //
+        //////////////////////////
 
-      // Use $stateProvider to configure your states.
-      $stateProvider
+        // Use $stateProvider to configure your states.
+        $stateProvider
 
         //////////
         // Home //
         //////////
 
-        .state("home", {
+          .state("home", {
 
           // Use a url of "/" to set a states as the "index".
           url: "/",
@@ -79,18 +78,19 @@ angular.module('uiRouterSample', [
 
           // Showing off how you could return a promise from templateProvider
           templateProvider: ['$timeout',
-            function (        $timeout) {
-              return $timeout(function () {
+            function($timeout) {
+              return $timeout(function() {
                 return '<p class="lead">UI-Router Resources</p><ul>' +
-                         '<li><a href="https://github.com/angular-ui/ui-router/tree/master/sample">Source for this Sample</a></li>' +
-                         '<li><a href="https://github.com/angular-ui/ui-router">Github Main Page</a></li>' +
-                         '<li><a href="https://github.com/angular-ui/ui-router#quick-start">Quick Start</a></li>' +
-                         '<li><a href="https://github.com/angular-ui/ui-router/wiki">In-Depth Guide</a></li>' +
-                         '<li><a href="https://github.com/angular-ui/ui-router/wiki/Quick-Reference">API Reference</a></li>' +
-                       '</ul>';
+                  '<li><a href="https://github.com/angular-ui/ui-router/tree/master/sample">Source for this Sample</a></li>' +
+                  '<li><a href="https://github.com/angular-ui/ui-router">Github Main Page</a></li>' +
+                  '<li><a href="https://github.com/angular-ui/ui-router#quick-start">Quick Start</a></li>' +
+                  '<li><a href="https://github.com/angular-ui/ui-router/wiki">In-Depth Guide</a></li>' +
+                  '<li><a href="https://github.com/angular-ui/ui-router/wiki/Quick-Reference">API Reference</a></li>' +
+                  '</ul>';
               }, 100);
-            }]
+            }
+          ]
         })
-    }
-  ]
-);
+      }
+    ]
+  );
